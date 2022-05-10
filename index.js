aaf37096d03ec2a19ebd93a57d9e9ceb0d9aa520
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-10 17:10:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,27 +27,23 @@ const mongoEventFiles = fs
   .readdirSync("./mongoEvents")
   .filter((file) => file.endsWith(".js"));
 
-module.exports = (client) => {
-  client.dbLogin = async () => {
-    for (file of mongoEventFiles) {
-      const event = require(`../mongoEvents/${file}`);
-      if (event.once) {
-        mongoose.connection.once(event.name, (...args) =>
-          event.execute(...args)
-        );
-      } else {
-        mongoose.connection.on(event.name, (...args) => event.execute(...args));
-      }
+dbLogin = async () => {
+  for (file of mongoEventFiles) {
+    const event = require(`../mongoEvents/${file}`);
+    if (event.once) {
+      mongoose.connection.once(event.name, (...args) => event.execute(...args));
+    } else {
+      mongoose.connection.on(event.name, (...args) => event.execute(...args));
     }
-    mongoose.Promise = global.Promise;
-    await mongoose.connect(process.env.dbToken, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-  };
+  }
+  mongoose.Promise = global.Promise;
+  await mongoose.connect(process.env.dbToken, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
 };
 
-client.dbLogin();
+dbLogin();
 
 // const clientId = "967030090875670598";
 // const DiscordRPC = require("discord-rpc");
