36c65b70a423e587685203da7a3d37d0286931b3
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-10 16:30:37 +0200

    update .gitignore, commands/developer/embed.js and objects.embed.js

--- objects/embeds.js
+++ objects/embeds.js
@@ -190,4 +190,50 @@ module.exports = {
     })
     .setColor("#ADD8E6")
     .setTimestamp(),
+  // Embed
+  information: new MessageEmbed()
+    .setTitle(`F15H server information`)
+    .addFields(
+      {
+        name: `Channels`,
+        value: `<#970349280873304084> - In this channel information about the server can be found.\n<#937019093960253540> - In this channel all the rules which you must obey can be found.\n<#970349111985455125> - In this channel tickets can be created to get server support.\n<#937018874572972115> - In this channel announcements regarding the server or bot will be made.\n<#937019071151603772> - In this channel updates to the bot will be logged.\n<#937055370944319498> - In this channel all the server boosts are logged.\n<#937019249648623656> - Chill with other users!.\n<#937019775433990144>, <#937019797248557127> and <#937019828764573707> - Go F15Hing!`,
+      },
+      {
+        name: `Roles`,
+        value: `<@&939517749267423284> - Our lord and savior.\n<@&937453959487049748> - People who develop F15H.\n<@&967883504522260603> - People who keep the server clean.\n<@&971139439055233024> - People who joined this server before the full release.\n<@&967883535430062131> - People who joined the server.`,
+      }
+    )
+    .setColor("#ADD8E6"),
+  rules: new MessageEmbed()
+    .setTitle(`F15H server rules`)
+    .addFields(
+      {
+        name: `Text channel rules`,
+        value: `1) Respect other users.\n2) Do not spam messages, including emojis, stickers and @mentions.\n3) Do not send messages which contain sexual, discriminatory, racial or political content\n4) Do not advertise social media, including in user's their dms.\n5) Do not @mention staff without a valid reason.\n6) Use channels for their intended purpose. You can find this in <#970349280873304084>.`,
+      },
+      {
+        name: `Voice channel rules`,
+        value: `1) Respect other users.\n2) Do not play loud or disturbing audio through your microphone.\n3) Do not use voice changers`,
+      },
+      {
+        name: `Other rules`,
+        value: `1) Staff has the final say on all situations. If you feel like they were unjust, create a <#970349111985455125>.\n2) Do not abuse any bugs of the bot. Doing so can result in a blacklist. Report them instead using /report.`,
+      },
+      {
+        name: `RULES ARE SUBJECT TO CHANGE ANYTIME`,
+        value: `If the rules are changed, we will announce it in <#937018874572972115>.`,
+      },
+      {
+        name: `IGNORANCE OF THE RULES IS NOT AN EXCUSE!`,
+        value: `You are obliged to know what you can and can't do.`,
+      }
+    )
+    .setFooter({ text: `Happy F15Hing! 🐟` })
+    .setColor("#ADD8E6"),
+  updateLog: new MessageEmbed(),
+  staffRules: new MessageEmbed(),
+  confirmation: new MessageEmbed()
+    .setTitle(`Embed sent!`)
+    .setColor("#ADD8E6")
+    .setTimestamp(),
 };
