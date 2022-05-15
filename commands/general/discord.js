84d4c7800bc8f9a6e1cba4bd0182df8e4e1c0240
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-15 19:01:34 +0200

    Fix /discord hyperlink

--- commands/general/discord.js
+++ commands/general/discord.js
@@ -9,7 +9,7 @@ module.exports = {
     const embed = new MessageEmbed()
       .setTitle(`F15H discord server link`)
       .setDescription(
-        `Click [here](https://discord.gg/a86CkcXjBC) to join the official F15H discord server!\n(code = qfnkjSzaK5)`
+        `Click [here](https://discord.gg/qfnkjSzaK5) to join the official F15H discord server!`
       )
       .setFooter({ text: `Requested by ${interaction.user.tag}` })
       .setColor("#ADD8E6")
