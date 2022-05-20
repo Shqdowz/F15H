ce40b4663082d75c65b2018209835526d5e6f1a2
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-20 19:55:05 +0200

    Update /blacklist and /whitelist logging

--- commands/developer/whitelist.js
+++ commands/developer/whitelist.js
@@ -11,21 +11,41 @@ module.exports = {
         .setName("user")
         .setDescription("The userID to whitelist")
         .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("reason")
+        .setDescription("The reason for the blacklist")
+        .setRequired(true)
     ),
   async execute(interaction, client) {
     if (interaction.user.id === "856545083310604308") {
       const user = interaction.options.getUser("user");
+      const reason = interaction.options.getString("reason");
+
+      const guild = client.guilds.cache.get("937018874572972112");
+      const channel = "976933257583144980";
 
       const userProfile = await client.createUser(user);
 
-      const embed = new MessageEmbed()
+      const logEmbed = new MessageEmbed()
+        .setTitle("Blacklist")
+        .setDescription(
+          `Developer ${interaction.user.tag} blacklisted ${user} for: ${reason}.`
+        )
+        .setColor("#FFFFFF")
+        .setTimestamp();
+      const confirmationEmbed = new MessageEmbed()
         .setTitle(`Whitelist successful!`)
-        .setDescription(`Whitelisted user ${user} (${user.id})`)
+        .setDescription(`Whitelisted user ${user} (${user.id}) for: ${reason}`)
         .setColor("#ADD8E6")
         .setTimestamp();
 
+      await guild.channels.cache.get(channel).send({
+        embeds: [logEmbed],
+      });
       await interaction.reply({
-        embeds: [embed],
+        embeds: [confirmationEmbed],
         ephemeral: true,
       });
 
