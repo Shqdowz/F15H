17f17aa4d4aabad4c8b1df9cc664fbd92e99f8a9
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-21 21:42:43 +0200

    Compact commands\market\shop.js

--- commands/market/shop.js
+++ commands/market/shop.js
@@ -1,18 +1,46 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
-const embedObject = require("../../objects/embeds");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("shop")
     .setDescription("Displays the shop"),
-  async execute(interaction, client) {
-    embedObject.shop.setFooter({
-      text: `Requested by ${interaction.user.tag}`,
-    });
+  async execute(interaction) {
+    // Embed
+
+    const shop = new MessageEmbed()
+      .setTitle(`F15H shop`)
+      .addFields(
+        {
+          name: "[1] Exquisite rod",
+          value:
+            "Cost: 2000 <:FishCoin:937423381756772364>\n- Greatly increases uncommon drop rate\n- Slightly increases rare, epic, mythic and legendary drop rates\n- Slightly decreases common drop rate",
+        },
+        {
+          name: "[2] Precious rod",
+          value:
+            "Cost: 4000 <:FishCoin:937423381756772364>\n- Greatly increases rare drop rate\n- Slightly increases epic, mythic and legendary drop rates\n- Slightly decreases common and uncommon drop rates",
+        },
+        {
+          name: "[3] Luxurious rod",
+          value:
+            "Cost: 8000 <:FishCoin:937423381756772364>\n- Greatly increases epic drop rate\n- Slightly increases mythic and legendary drop rates\n- Slightly decreases common, uncommon and rare drop rates",
+        },
+        {
+          name: "[4] Divine rod",
+          value:
+            "Cost: 16000 <:FishCoin:937423381756772364>\n- Greatly increases mythic drop rate\n- Slightly increases legendary drop rate\n- Slightly decreases common, uncommon, rare and epic drop rates",
+        }
+      )
+      .setFooter({ text: `Requested by ${interaction.user.tag}` })
+      .setColor("#ADD8E6")
+      .setTimestamp();
+
+    // Code
+
     await interaction.reply({
       content: `<@${interaction.user.id}>`,
-      embeds: [embedObject.shop],
+      embeds: [shop],
     });
   },
 };
