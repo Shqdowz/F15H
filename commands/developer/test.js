6499de2c258b665cfd3b073ff20a01457c7c2cd0
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-23 17:33:59 +0200

    Update test.js

---
+++ commands/developer/test.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("(DEV) Test an output"),
+  async execute(interaction, client) {
+    if (interaction.user.id === "856545083310604308") {
+      const invites = [];
+      const guilds = client.guilds.cache.map((guild) => guild.id);
+      guilds.forEach((guild) => {
+        try {
+          const channel = guild.channels.cache
+            .filter((channel) => channel.type === "text")
+            .first();
+          await channel
+            .createInvite({ maxAge: 0, maxUses: 0 })
+            .then(async (invite) => {
+              invites.push(`${guild.name} - ${invite.url}`);
+            });
+          console.log(invites);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    } else {
+      await interaction.reply({
+        content: `You are not allowed to use this command!`,
+        ephemeral: true,
+      });
+    }
+  },
+};
