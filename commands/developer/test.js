edaa44a4bead8bf66c492977b6f00bc73956a1a6
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-23 18:05:51 +0200

    Update test.js

--- commands/developer/test.js
+++ commands/developer/test.js
@@ -7,23 +7,7 @@ module.exports = {
     .setDescription("(DEV) Test an output"),
   async execute(interaction, client) {
     if (interaction.user.id === "856545083310604308") {
-      const invites = [];
-      const guilds = client.guilds.cache.map((guild) => guild.id);
-      guilds.forEach(async (guild) => {
-        try {
-          const channel = guild.channels
-            .filter((channel) => channel.type === "text")
-            .first();
-          await channel
-            .createInvite({ maxAge: 0, maxUses: 0 })
-            .then(async (invite) => {
-              invites.push(`${guild.name} - ${invite.url}`);
-            });
-          console.log(invites);
-        } catch (err) {
-          console.log(err);
-        }
-      });
+      console.log(client.guilds.cache.map((guild) => guild.name));
     } else {
       await interaction.reply({
         content: `You are not allowed to use this command!`,
