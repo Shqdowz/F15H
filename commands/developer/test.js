699857dc9a7cc30ad6508965a6fe6c0281533f8b
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-05-24 22:20:43 +0200

    Update test.js

--- commands/developer/test.js
+++ commands/developer/test.js
@@ -6,7 +6,14 @@ module.exports = {
     .setName("test")
     .setDescription("(DEV) Test an output"),
   async execute(interaction, client) {
-    const guildNames = client.guilds.cache.map((guild) => guild.name);
-    interaction.reply({ content: `${guildNames}`, ephemeral: true });
+    if (interaction.user.id === "856545083310604308") {
+      const guildNames = client.guilds.cache.map((guild) => guild.name);
+      interaction.reply({ content: `${guildNames}`, ephemeral: true });
+    } else {
+      await interaction.reply({
+        content: `You are not allowed to use this command!`,
+        ephemeral: true,
+      });
+    }
   },
 };
