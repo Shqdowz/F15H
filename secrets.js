f479bb0736589eaee6b2458a2bcf2e785d63e5d1
Shqdowz <86895396+Shqdowz@users.noreply.github.com>
2022-06-07 23:34:49 +0200

    /checkin maintenance

---
+++ secrets.js
@@ -0,0 +1,47 @@
+// Only push when updated before
+
+// To private: (update ->) push to git -> push to heroku -> delete file -> push to git -> create file
+
+module.exports = {
+  // Fish drop rates
+  commonRod: {
+    commonChance: 876,
+    uncommonChance: 940,
+    rareChance: 972,
+    epicChance: 988,
+    mythicChance: 996,
+    legendaryChance: 1000,
+  },
+  exquisiteRod: {
+    commonChance: 617,
+    uncommonChance: 925,
+    rareChance: 965,
+    epicChance: 985,
+    mythicChance: 995,
+    legendaryChance: 1000,
+  },
+  preciousRod: {
+    commonChance: 547,
+    uncommonChance: 821,
+    rareChance: 958,
+    epicChance: 982,
+    mythicChance: 994,
+    legendaryChance: 1000,
+  },
+  luxuriousRod: {
+    commonChance: 522,
+    uncommonChance: 783,
+    rareChance: 914,
+    epicChance: 979,
+    mythicChance: 993,
+    legendaryChance: 1000,
+  },
+  divineRod: {
+    commonChance: 512,
+    uncommonChance: 768,
+    rareChance: 896,
+    epicChance: 960,
+    mythicChance: 992,
+    legendaryChance: 1000,
+  },
+};
